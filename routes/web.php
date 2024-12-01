2bef163789325c2f8133f2e8b60b203e4621e8fa
goldistry <goldistry99@gmail.com>
2024-12-01 18:53:51 +0700

    FEAT: PROFILE

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-        return view('register', ['title' => 'coba']);
+        return view('profile', ['title' => 'coba']);
     });
 Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/regist', [AuthController::class, 'regist'])->name('regist');
