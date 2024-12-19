73550dc5a46657c9d4fe828fe1393676add95b77
XwcdX <darkpuppet90@gmail.com>
2024-12-19 12:34:02 +0700

    fix: web.php

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,10 @@ use Illuminate\Support\Facades\Route;
 //     return view('home', ['title' => 'coba']);
 // });
 // Route::get('/home', [UserController::class, 'home'])->name('home');
-Route::get('/popular', [MainController::class, 'popular'])->name('popular');
-Route::get('/ask', [UserController::class, 'askPage'])->name('askPage');
-Route::get('/viewUser/{email}', [MainController::class, 'viewOther'])->name('viewOthers');
 
 Route::get('/loginOrRegist', [AuthController::class, 'loginOrRegist'])->name('loginOrRegist');
 Route::post('/manualLogin', [AuthController::class, 'manualLogin'])->name('manualLogin');
-Route::post('/submitRegister', [AuthController::class,'submitRegister'])->name('submitRegister');
+Route::post('/submitRegister', [AuthController::class, 'submitRegister'])->name('submitRegister');
 Route::get('/email/verify', [AuthController::class, 'verifyEmail'])->name('email.verify');
 Route::post('/email/resend', [AuthController::class, 'resendVerificationEmail'])->name('verification.resend');
 
@@ -25,24 +22,26 @@ Route::get('/auth', [AuthController::class, 'googleAuth'])->name('auth');
 Route::get('/process/login', [AuthController::class, 'processLogin'])->name('processLogin');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::get('/home', [MainController::class, 'home'])->name('home');
+
 
 Route::middleware(['isLogin'])->group(function () {
+    Route::get('/popular', [MainController::class, 'popular'])->name('popular');
+    Route::get('/ask', [UserController::class, 'askPage'])->name('askPage');
+    Route::get('/viewUser/{email}', [MainController::class, 'viewOther'])->name('viewOthers');
+
     // Route::get('/{id}', [UserController::class, 'viewOther']);
     Route::post('/follow', [UserController::class, 'nembakFollow'])->name('nembakFollow');
+    Route::get('/', [MainController::class, 'home'])->name('home');
+
+    Route::get('/myProfile', [UserController::class, 'seeProfile'])->name('seeProfile');
+    Route::get('/editProfile', [UserController::class, 'editProfile'])->name('editProfile');
+
+    // view questions
+    Route::get('/viewUsers', [MainController::class, 'viewAllUsers'])->name('viewAllUsers');
+    Route::get('/viewAnswers/{questionId}', [MainController::class, 'viewAnswers'])->name('user.viewQuestions');
+    Route::get('/viewTags', [UserController::class, 'viewTags'])->name('viewAllTags');
+    Route::get('/viewUser/{email}', [MainController::class, 'viewUser'])->name('viewUser');
+    Route::post('/submitAnswer/{questionId}', [AnswerController::class, 'submitAnswer'])->name('submitAnswer');
+    Route::post('/addQuestion', [QuestionController::class, 'addQuestion'])->name('addQuestion');
+    Route::post('/submit/question/comment/{questionId}', [QuestionController::class, 'submitQuestionComment'])->name('question.comment.submit');
 });
-
-Route::get('/myProfile', [UserController::class, 'seeProfile'])->name('seeProfile');
-Route::get('/editProfile', [UserController::class, 'editProfile'])->name('editProfile');
-
-// view questions
-Route::get('/viewUsers', [MainController::class, 'viewAllUsers'])->name('viewAllUsers');
-Route::get('/viewAnswers/{questionId}', [MainController::class, 'viewAnswers'])->name('user.viewQuestions');
-Route::get('/viewTags', [UserController::class, 'viewTags'])->name('viewAllTags');
-Route::get('/viewUser/{email}', [MainController::class, 'viewUser'])->name('viewUser');
-Route::post('/submitAnswer/{questionId}', [AnswerController::class, 'submitAnswer'])->name('submitAnswer');
-Route::post('/addQuestion', [QuestionController::class, 'addQuestion'])->name('addQuestion');
-Route::post('/submit/question/comment/{questionId}', [QuestionController::class, 'submitQuestionComment'])->name('question.comment.submit');
-
-// view tags
-
